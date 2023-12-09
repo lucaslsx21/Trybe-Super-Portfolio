98fa781b1ca748d5013b78b35cebdf9074cb0757
lucaslx21 <lucaslsilva@live.com>
2023-12-09 10:59:32 -0300

    C.R.U.D inline para Certificate e CertifyingInstitution e Exibir uma página de perfil completa-REQ5 e REQ6

--- projects/serializers.py
+++ projects/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Profile, Project
+from .models import Profile, Project, CertifyingInstitution, Certificate
 
 
 class ProfileSerializer(serializers.ModelSerializer):
@@ -11,4 +11,39 @@ class ProfileSerializer(serializers.ModelSerializer):
 class ProjectSerializer(serializers.ModelSerializer):
     class Meta:
         model = Project
+        fields = "__all__"
+
+
+class NestedCertificatesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Certificate
+        fields = ["id", "name", "timestamp"]
+
+
+class CertifyingInstitutionSerializer(serializers.ModelSerializer):
+    certificates = NestedCertificatesSerializer(many=True)
+
+    class Meta:
+        model = CertifyingInstitution
+        fields = ["id", "name", "url", "certificates"]
+
+    def create(self, validated_data):
+        certificates_data = validated_data.pop("certificates")
+        new_certifying_institution = CertifyingInstitution.objects.create(
+            **validated_data
+        )
+        new_certificate = {}
+        for certificate in certificates_data:
+            new_certificate = {
+                "name": certificate["name"],
+                "certifying_institution": new_certifying_institution,
+                "profiles": [],
+            }
+            CertificateSerializer().create(new_certificate)
+        return new_certifying_institution
+
+
+class CertificateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Certificate
         fields = "__all__"
--- projects/urls.py
+++ projects/urls.py
@@ -1,14 +1,24 @@
 from rest_framework import routers
 from django.urls import path, include
 
-from .views import ProfileViewSet, ProjectViewSet
+from .views import (
+    ProfileViewSet,
+    ProjectViewSet,
+    CertifyingInstitutionViewSet,
+    CertificateViewSet,
+)
 
 
 router = routers.DefaultRouter()
+
 router.register(r"profiles", ProfileViewSet)
 router.register(r"profiles/<int: id>", ProfileViewSet)
+
 router.register(r"projects", ProjectViewSet)
 router.register(r"projects/<int: id>", ProjectViewSet)
+
+router.register(r"certifying-institutions", CertifyingInstitutionViewSet)
+router.register(r"certificates", CertificateViewSet)
 
 
 urlpatterns = [
